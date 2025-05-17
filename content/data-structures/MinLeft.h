fc8f39f6c5a4da8edcf485b972112233cf2a5d82
Xylenox <37784637+Xylenox@users.noreply.github.com>
2025-05-17 14:21:19 -0400

    Update MinLeft.h (#65)

--- content/data-structures/MinLeft.h
+++ content/data-structures/MinLeft.h
@@ -13,14 +13,13 @@ int min_left(int r, auto g) {
 	dip push((r - 1) >> i);
 	S sm = ego();
 	do {
-		r--;
-		while (r > 1 && (r % 2)) r /= 2;
+		for (r--; r > 1 && (r % 2);) r /= 2;
 		if (!g(op(d[r], sm))) {
-		while (r < size) {
-			push(r);
-			if (g(op(d[r = 2*r+1], sm))) sm = op(d[r--], sm);
-		}
-		return r + 1 - size;
+			while (r < size) {
+				push(r);
+				if (g(op(d[r = 2*r+1], sm))) sm = op(d[r--], sm);
+			}
+			return r + 1 - size;
 		}
 		sm = op(d[r], sm);
 	} while ((r & -r) != r);
