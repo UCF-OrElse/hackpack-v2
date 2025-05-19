517b6d53e66b9e1d4ffb1769ca677696dd4c8349
Andy Phan <andytphan@outlook.com>
2025-05-18 23:53:37 -0400

    convert indentation to tabs

--- content/graphs/KthPath.h
+++ content/graphs/KthPath.h
@@ -6,11 +6,11 @@
  * Time: $O(\log N)$
  */
 int kth_path(int u, int v, int k) {
-    int lca_d = t[lca(u, v)].d;
-    int u_lca = t[u].d - lca_d;
-    int v_lca = t[v].d - lca_d;
-    if (k <= u_lca) return kth_par(u, k);
-    if (k <= u_lca + v_lca)
-        return kth_par(v, u_lca + v_lca - k);
-    return -1;
+	int lca_d = t[lca(u, v)].d;
+	int u_lca = t[u].d - lca_d;
+	int v_lca = t[v].d - lca_d;
+	if (k <= u_lca) return kth_par(u, k);
+	if (k <= u_lca + v_lca)
+		return kth_par(v, u_lca + v_lca - k);
+	return -1;
 }
